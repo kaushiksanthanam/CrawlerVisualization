ea450d3d33ef79909fa02f0a42368ee5504fc093
Kaushik Santhanam <doors2kaushik@gmail.com>
2016-03-25 19:28:00 -0400

    New commit

--- CrawlerVisualization/CrawlerVisualization/MainUI.cs
+++ CrawlerVisualization/CrawlerVisualization/MainUI.cs
@@ -7,6 +7,16 @@ namespace CrawlerVisualizationUI
         public MainUI()
         {
             InitializeComponent();
+            //Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //Graph g = new Graph();
+            //g.AddNode(new Node("www.google.com"));
+            //g.AddNode(new Node("www.gatech.edu"));
+            //g.AddNode(new Node("www.cc.gatech.edu"));
+            //g.AddEdge("www.google.com", "www.gatech.edu");
+            //g.AddEdge("www.google.com", "www.cc.gatech.edu");
+            //viewer.Graph = g;
+            //this.Controls.Add(viewer);
+
         }
     }
 }
